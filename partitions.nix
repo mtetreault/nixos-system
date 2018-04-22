0fd1b46de13a7db5ad59ba27c7286823aa2fd191
mtetreault <mathieu.alexandre.tetreault@gmail.com>
2018-04-22 13:49:22 -0400

    partitions: automatically mount samba shares

--- partitions.nix
+++ partitions.nix
@@ -18,5 +18,65 @@
       label = "vms";
       fsType = "ext4";
     }
+    {
+      mountPoint = "/mnt/smb/console";
+      device = "//axel-parolta-desktop.local/console";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
+    {
+      mountPoint = "/mnt/smb/ebook";
+      device = "//axel-parolta-desktop.local/ebook";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
+    {
+      mountPoint = "/mnt/smb/famille";
+      device = "//axel-parolta-desktop.local/famille";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
+    {
+      mountPoint = "/mnt/smb/mtetreault";
+      device = "//axel-parolta-desktop.local/mtetreault";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
+    {
+      mountPoint = "/mnt/smb/software";
+      device = "//axel-parolta-desktop.local/software";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
+    {
+      mountPoint = "/mnt/smb/video";
+      device = "//axel-parolta-desktop.local/video";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    }
   ];
 }
