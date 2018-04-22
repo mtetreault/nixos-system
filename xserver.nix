c70b482c2e2ec69154ef6f1771376da11307cd66
mtetreault <mathieu.alexandre.tetreault@gmail.com>
2018-04-22 13:46:05 -0400

    xserver: enable thunar archive plugin

--- xserver.nix
+++ xserver.nix
@@ -3,9 +3,11 @@
 {
   services.xserver = {
     enable = true;
-    videoDrivers = [ "nvidia" ];
 
-    desktopManager.xfce.enable = true;
+    desktopManager.xfce = {
+      enable = true;
+      thunarPlugins = [ pkgs.xfce.thunar-archive-plugin ];
+    };
     displayManager.sddm.enable = true;
     displayManager.sddm.autoNumlock = true;
   };
