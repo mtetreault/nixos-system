333cbf72b629adef3b2666cb4cf44029031f2075
mtetreault <mathieu.alexandre.tetreault@gmail.com>
2018-04-26 07:22:44 -0400

    smb client: mount with uid:mtetreault,gid: user allowing rw without sudo rights

--- partitions.nix
+++ partitions.nix
@@ -22,61 +22,37 @@
       mountPoint = "/mnt/smb/console";
       device = "//mtetreault-desktop.local/console";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
     {
       mountPoint = "/mnt/smb/ebook";
       device = "//mtetreault-desktop.local/ebook";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
     {
       mountPoint = "/mnt/smb/famille";
       device = "//mtetreault-desktop.local/famille";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
     {
       mountPoint = "/mnt/smb/mtetreault";
       device = "//mtetreault-desktop.local/mtetreault";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
     {
       mountPoint = "/mnt/smb/software";
       device = "//mtetreault-desktop.local/software";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
     {
       mountPoint = "/mnt/smb/video";
       device = "//mtetreault-desktop.local/video";
       fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      options = ["credentials=/etc/nixos/smb-secrets,uid=mtetreault,gid=users"];
     }
   ];
 }
